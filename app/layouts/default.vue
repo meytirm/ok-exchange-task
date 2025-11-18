cee59ec2dad0c88a630b8ea476a1a60b7af37469
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-18 12:51:40 +0330

    feat(layouts): add default layout with container and slot structure

---
+++ app/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="">
+    <main class="container">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
