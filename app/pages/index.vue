c0a504fd58ca38fc3528294b84ac05153387d34b
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-18 20:50:08 +0330

    refactor(pages): replace inline `index` content with `IndexPage` component for better modularity

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,9 @@
 <template>
-  <div>
-    index
-  </div>
+  <IndexPage />
 </template>
 
 <script setup lang="ts">
-
+import IndexPage from '~/components/page/index/IndexPage.vue'
 </script>
 
 <style scoped>
