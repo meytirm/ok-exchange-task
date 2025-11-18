a171b3a298fba046b57dc18684df6e58139b4d78
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-18 09:33:03 +0330

    chore(config): integrate eslint module in Nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+
   typescript: {
     typeCheck: true,
   },
+
+  modules: ['@nuxt/eslint'],
 })
