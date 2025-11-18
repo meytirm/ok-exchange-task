d74c2f8aa5c324f1a5dcca44bfd4571261da1398
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-18 13:57:42 +0330

    feat(components): add `TopHeaderNavigationMenu` component with menu and sub-menu structure

---
+++ app/components/header/TopHeaderNavigationMenu.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <NavigationMenu
+      disable-hover-trigger
+      disable-pointer-leave-close
+    >
+      <NavigationMenuList>
+        <NavigationMenuItem
+          v-for="menuItem in topHeaderNavigationMenu"
+          :key="menuItem.label"
+        >
+          <NavigationMenuTrigger class="top-header-navigation-menu-trigger">
+            {{ menuItem.label }}
+          </NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <NavigationMenuLink
+              v-for="subMenuItem in menuItem.children"
+              :key="subMenuItem.title"
+            >
+              <img :src="subMenuItem.icon">
+              {{ subMenuItem.title }}
+            </NavigationMenuLink>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from '@/components/ui/navigation-menu'
+import { topHeaderNavigationMenu } from '~/constants/top-header-navigation-menu'
+</script>
+
+<style scoped>
+
+</style>
