c406bd92246c1bf7da3adb437ccc04b78c6c49a2
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-18 13:57:50 +0330

    refactor(components): move `TopHeader` to `header` folder and replace content with `TopHeaderNavigationMenu`

--- app/components/header/TopHeader.vue
+++ app/components/header/TopHeader.vue
@@ -1,11 +1,11 @@
 <template>
   <header class="top-header">
-    header
+    <TopHeaderNavigationMenu />
   </header>
 </template>
 
 <script setup lang="ts">
-
+import TopHeaderNavigationMenu from '~/components/header/TopHeaderNavigationMenu.vue'
 </script>
 
 <style scoped>
