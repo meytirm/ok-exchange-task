0b80a3a04ae2e537680f267f47975f6376c0aa55
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-18 16:04:11 +0330

    feat(components): integrate `TopHeaderActions` into `TopHeader` component

--- app/components/header/TopHeader.vue
+++ app/components/header/TopHeader.vue
@@ -1,11 +1,13 @@
 <template>
   <header class="top-header">
     <TopHeaderNavigationMenu />
+    <TopHeaderActions />
   </header>
 </template>
 
 <script setup lang="ts">
 import TopHeaderNavigationMenu from '~/components/header/TopHeaderNavigationMenu.vue'
+import TopHeaderActions from '~/components/header/TopHeaderActions.vue'
 </script>
 
 <style scoped>
