356c64f046ca786d3534cd191496e0d55903802a
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-18 09:53:33 +0330

    chore(config): extend shadcn-nuxt configuration with prefix and componentDir options

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,4 +23,18 @@ export default defineNuxtConfig({
       },
     },
   },
+  shadcn: {
+    /**
+     * Prefix for all the imported component.
+     * @default "Ui"
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * Will respect the Nuxt aliases.
+     * @link https://nuxt.com/docs/api/nuxt-config#alias
+     * @default "@/components/ui"
+     */
+    componentDir: '@/components/ui',
+  },
 })
