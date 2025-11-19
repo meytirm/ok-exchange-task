93c5fbd755422d74d795f77780a5da0280b6b8cd
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-19 19:26:19 +0330

    feat(components): add `CardSection` component for advanced crypto trading tools

---
+++ app/components/page/index/card/CardSection.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    <SectionTitle>
+      Advanced Crypto Trading Tools for Faster, Smarter Transactions
+    </SectionTitle>
+    <div class="mt-8">
+      <FutureCards />
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import SectionTitle from '~/components/common/SectionTitle.vue'
+import FutureCards from '~/components/page/index/card/FutureCards.vue'
+</script>
+
+<style scoped>
+
+</style>
