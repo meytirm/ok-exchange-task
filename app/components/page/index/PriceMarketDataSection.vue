032a4fe805a386fdc5cfd9c48930a3e2907b021f
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-19 15:20:14 +0330

    feat(components): add `PriceMarketDataSection` layout in `IndexPage` for cryptocurrency market data display

---
+++ app/components/page/index/PriceMarketDataSection.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <SectionTitle>
+      Live Cryptocurrency Prices Market Data
+    </SectionTitle>
+    <SectionSubtitle>
+      Top Gainers Today
+    </SectionSubtitle>
+    <div
+      class="grid grid-cols-3 gap-5"
+    >
+      <div class="col-span-2">
+        <CryptoListTab />
+      </div>
+      <div class="col-span-1">
+        2
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import SectionTitle from '~/components/common/SectionTitle.vue'
+import CryptoListTab from '~/components/common/CryptoListTab.vue'
+import SectionSubtitle from '~/components/common/SectionSubtitle.vue'
+</script>
+
+<style scoped>
+
+</style>
