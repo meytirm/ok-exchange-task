318a4898a432c1ed610fc54fd68dead3aeb011a3
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-19 14:30:09 +0330

    feat(components): add `PriceMarketDataSection` to `IndexPage` for displaying pricing information

--- app/components/page/index/IndexPage.vue
+++ app/components/page/index/IndexPage.vue
@@ -2,12 +2,14 @@
   <div>
     <IntroduceSection />
     <WhyUsSection />
+    <PriceMarketDataSection />
   </div>
 </template>
 
 <script setup lang="ts">
 import IntroduceSection from '~/components/page/index/IntroduceSection.vue'
 import WhyUsSection from '~/components/page/index/why-us/WhyUsSection.vue'
+import PriceMarketDataSection from '~/components/page/index/PriceMarketDataSection.vue'
 </script>
 
 <style scoped>
