71284f7d4c08a8786ab1abee971b6a7771411d66
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-19 19:26:33 +0330

    feat(components): add `FutureCards` component for displaying multiple futures trading options

---
+++ app/components/page/index/card/FutureCards.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="future-cards">
+    <FutureCard
+      :image="CardImage"
+      action-text=""
+      title="Futures Trading with Up to 100x Leverage"
+      description="Trade Bitcoin, Ethereum, and other perpetual swap contracts with up to 100x leverage on Poloniex Futures."
+    />
+    <FutureCard
+      :image="CardImage"
+      action-text="30x max leverage"
+      title="Futures Trading with Up to 100x Leverage"
+      description="Trade Bitcoin, Ethereum, and other perpetual swap contracts with up to 100x leverage on Poloniex Futures."
+    />
+    <FutureCard
+      :image="CardImage"
+      action-text=""
+      title="Futures Trading with Up to 100x Leverage"
+      description="Trade Bitcoin, Ethereum, and other perpetual swap contracts with up to 100x leverage on Poloniex Futures."
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import CardImage from '@/assets/images/25x.png'
+import FutureCard from '~/components/common/FutureCard.vue'
+</script>
+
+<style scoped>
+
+</style>
