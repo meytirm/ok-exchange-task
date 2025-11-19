bc50f717ae09999af2c24a6a6db6a8d8699d7eeb
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-19 16:14:14 +0330

    feat(components): add `TopThreeTable` to `PriceMarketDataSection` for displaying top 3 losers

--- app/components/page/index/PriceMarketDataSection.vue
+++ app/components/page/index/PriceMarketDataSection.vue
@@ -12,8 +12,21 @@
       <div class="col-span-2">
         <CryptoListTab />
       </div>
-      <div class="col-span-1">
-        2
+      <div class="col-span-1 flex flex-col gap-5">
+        <div class="grow bg-white-new-100 rounded-2xl p-4 flex flex-col">
+          <div class="text-xl leading-9 font-semibold text-grey-new-900">
+            Top 3 Losers
+          </div>
+          <hr class="mb-4 border-white-new-300">
+          <TopThreeTable class="grow" />
+        </div>
+        <div class="grow bg-white-new-100 rounded-2xl p-4 flex flex-col">
+          <div class="text-xl leading-9 font-semibold text-grey-new-900">
+            Top 3 Losers
+          </div>
+          <hr class="mb-4 border-white-new-300">
+          <TopThreeTable class="grow" />
+        </div>
       </div>
     </div>
   </section>
@@ -23,6 +36,7 @@
 import SectionTitle from '~/components/common/SectionTitle.vue'
 import CryptoListTab from '~/components/common/CryptoListTab.vue'
 import SectionSubtitle from '~/components/common/SectionSubtitle.vue'
+import TopThreeTable from '~/components/common/TopThreeTable.vue'
 </script>
 
 <style scoped>
