7d91c2474efc10262ea36a3fd51f53ae8f0ac001
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-20 09:13:32 +0330

    feat(components): add `TellAboutUsCard` component with props for icon, title, and description

---
+++ app/components/page/index/tell-about-us/TellAboutUsCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <BaseCard class="flex flex-col gap-2 bg-white-new-100">
+    <div class="flex items-center gap-2">
+      <img
+        :src="icon"
+        alt="icon"
+        class="w-[20px]"
+      >
+      <div class="text-sm leading-6 text-grey-new-900 font-medium">
+        {{ title }}
+      </div>
+    </div>
+    <div class="text-base leading-7 text-grey-new-100">
+      {{ description }}
+    </div>
+  </BaseCard>
+</template>
+
+<script setup lang="ts">
+import BaseCard from '~/components/ui/card/BaseCard.vue'
+
+defineProps<{
+  icon: string
+  title: string
+  description: string
+}>()
+</script>
+
+<style scoped>
+
+</style>
