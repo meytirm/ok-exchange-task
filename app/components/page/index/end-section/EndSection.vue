a23d47ae4470f5fa64e3e50de6e422c51f243cf3
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-20 10:22:26 +0330

    feat(index-page): integrate `EndSection` component into IndexPage

---
+++ app/components/page/index/end-section/EndSection.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="mt-20">
+    <BitcoinSection />
+  </div>
+</template>
+
+<script setup lang="ts">
+import BitcoinSection from '~/components/page/index/end-section/BitcoinSection.vue'
+</script>
+
+<style scoped>
+
+</style>
