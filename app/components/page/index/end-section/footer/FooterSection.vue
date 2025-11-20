9d7432e901c0bfba61969a1e6e5dd06abfa992b6
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-20 13:31:04 +0330

    feat(footer): add `ConnectUsSection` component to `FooterSection`

--- app/components/page/index/end-section/footer/FooterSection.vue
+++ app/components/page/index/end-section/footer/FooterSection.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
     <FooterMenu />
+    <ConnectUsSection class="mt-4" />
   </div>
 </template>
 
 <script setup lang="ts">
 import FooterMenu from '~/components/page/index/end-section/footer/FooterMenu.vue'
+import ConnectUsSection from '~/components/page/index/end-section/footer/ConnectUsSection.vue'
 </script>
 
 <style scoped>
