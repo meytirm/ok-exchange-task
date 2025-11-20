885343083e883a01cb464195e9f5ef8cb3bcae3e
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-20 11:38:50 +0330

    feat(index-page): include `FooterSection` in `EndSection` component

--- app/components/page/index/end-section/EndSection.vue
+++ app/components/page/index/end-section/EndSection.vue
@@ -2,12 +2,14 @@
   <div class="mt-20 flex flex-col gap-20">
     <BitcoinSection />
     <FrequentlyQuestionsSection />
+    <FooterSection />
   </div>
 </template>
 
 <script setup lang="ts">
 import BitcoinSection from '~/components/page/index/end-section/BitcoinSection.vue'
 import FrequentlyQuestionsSection from '~/components/page/index/end-section/FrequentlyQuestionsSection.vue'
+import FooterSection from '~/components/page/index/end-section/footer/FooterSection.vue'
 </script>
 
 <style scoped>
