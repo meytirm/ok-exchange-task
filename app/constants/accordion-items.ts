1f96b27185c45b7bb3e9cc655f97c4cb60df57b0
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-20 11:08:26 +0330

    feat(constants): add `accordionItems` constant for reusable accordion data

---
+++ app/constants/accordion-items.ts
@@ -0,0 +1,33 @@
+import type { AccordionItem } from '~~/types'
+
+export const accordionItems: AccordionItem[] = [
+  {
+    title: 'What is a cryptocurrency exchange?',
+    content: 'Users can earn rewards on more than 180+ cryptocurrencies by using one of the products offered on Binance Earn. Our platform offers dozens of digital assets like Bitcoin, Ethereum, and stablecoins.',
+  },
+  {
+    title: 'What products does Binance provide?',
+    content: 'Users can earn rewards on more than 180+ cryptocurrencies by using one of the products offered on Binance Earn. Our platform offers dozens of digital assets like Bitcoin, Ethereum, and stablecoins.',
+  },
+  {
+    title: 'How to buy Bitcoin and other cryptocurrencies on Binance',
+    content: 'Users can earn rewards on more than 180+ cryptocurrencies by using one of the products offered on Binance Earn. Our platform offers dozens of digital assets like Bitcoin, Ethereum, and stablecoins.',
+  },
+  {
+    title: 'How to track cryptocurrency prices',
+    content: 'Users can earn rewards on more than 180+ cryptocurrencies by using one of the products offered on Binance Earn. Our platform offers dozens of digital assets like Bitcoin, Ethereum, and stablecoins.',
+  },
+  {
+    title: 'How to trade cryptocurrencies on Binance',
+    content: 'Users can earn rewards on more than 180+ cryptocurrencies by using one of the products offered on Binance Earn. Our platform offers dozens of digital assets like Bitcoin, Ethereum, and stablecoins.',
+  },
+  {
+    title: 'How to earn from crypto on Binance',
+    content: 'Users can earn rewards on more than 180+ cryptocurrencies by using one of the products offered on Binance Earn. Our platform offers dozens of digital assets like Bitcoin, Ethereum, and stablecoins.',
+  },
+  {
+    title: 'What is a cryptocurrency exchange?',
+    content: 'Users can earn rewards on more than 180+ cryptocurrencies by using one of the products offered on Binance Earn. Our platform offers dozens of digital assets like Bitcoin, Ethereum, and stablecoins.',
+  },
+
+]
