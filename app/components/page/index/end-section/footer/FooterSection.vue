9f3164f78bb75552135f81736d0952a287ac0133
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-20 11:38:44 +0330

    feat(index-page): add `FooterSection` to `EndSection` component

---
+++ app/components/page/index/end-section/footer/FooterSection.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <FooterMenu />
+  </div>
+</template>
+
+<script setup lang="ts">
+import FooterMenu from '~/components/page/index/end-section/footer/FooterMenu.vue'
+</script>
+
+<style scoped>
+
+</style>
