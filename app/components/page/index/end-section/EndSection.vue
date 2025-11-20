f9cba99cfb5408038c3a86f2293aa86817d5ad87
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-20 11:08:48 +0330

    feat(index-page): add `FrequentlyQuestionsSection` to `EndSection` component

--- app/components/page/index/end-section/EndSection.vue
+++ app/components/page/index/end-section/EndSection.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="mt-20">
+  <div class="mt-20 flex flex-col gap-20">
     <BitcoinSection />
+    <FrequentlyQuestionsSection />
   </div>
 </template>
 
 <script setup lang="ts">
 import BitcoinSection from '~/components/page/index/end-section/BitcoinSection.vue'
+import FrequentlyQuestionsSection from '~/components/page/index/end-section/FrequentlyQuestionsSection.vue'
 </script>
 
 <style scoped>
