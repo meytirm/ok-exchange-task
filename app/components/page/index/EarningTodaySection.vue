33e9e9f85e048f41fa33edbcf2f1a649a9cfacbf
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-20 09:39:08 +0330

    feat(index-page): add `EarningTodaySection` with call-to-action components

---
+++ app/components/page/index/EarningTodaySection.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="p-6">
+    <div
+      class="bg-white-new-400 absolute left-0 w-full flex flex-col items-center justify-center gap-2 lg:p-6 p-4"
+    >
+      <SectionTitle>Start Earning Today</SectionTitle>
+      <BrandButton
+        size="lg"
+        class="px-9"
+      >
+        Sign Up
+      </BrandButton>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import SectionTitle from '~/components/common/SectionTitle.vue'
+import BrandButton from '~/components/common/buttons/BrandButton.vue'
+</script>
+
+<style scoped>
+
+</style>
