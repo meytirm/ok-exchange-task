1749d456904b7a1adbeec1d430fbea874476eb2c
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-20 09:13:59 +0330

    feat(index-page): add `TellAboutUsSection` with cards and responsive carousel

---
+++ app/components/page/index/tell-about-us/TellAboutUsSection.vue
@@ -0,0 +1,38 @@
+<template>
+  <section>
+    <SectionTitle>
+      What do users tell about us?
+    </SectionTitle>
+    <div class="flex justify-between gap-5 mt-8 lg:flex hidden">
+      <TellAboutUsCard
+        title="Tony Marshal"
+        :icon="UserIcon"
+        description="Trade Bitcoin, Ethereum, and other perpetual swap contracts with up to 100x leverage on Poloniex Futures."
+      />
+      <TellAboutUsCard
+        title="Tony Marshal"
+        :icon="UserIcon"
+        description="Trade Bitcoin, Ethereum, and other perpetual swap contracts with up to 100x leverage on Poloniex Futures."
+      />
+      <TellAboutUsCard
+        title="Tony Marshal"
+        :icon="UserIcon"
+        description="Trade Bitcoin, Ethereum, and other perpetual swap contracts with up to 100x leverage on Poloniex Futures."
+      />
+    </div>
+    <div class="lg:hidden block mt-8">
+      <TellAboutUsCarousel />
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import SectionTitle from '~/components/common/SectionTitle.vue'
+import TellAboutUsCard from '~/components/page/index/tell-about-us/TellAboutUsCard.vue'
+import UserIcon from '~/assets/icons/user.svg'
+import TellAboutUsCarousel from '~/components/page/index/tell-about-us/TellAboutUsCarousel.vue'
+</script>
+
+<style scoped>
+
+</style>
